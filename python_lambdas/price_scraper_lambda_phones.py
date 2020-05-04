500036c9114dddba9662522c1abfc2bb10c17e85
hlatimer266 <hlatimer266@gmail.com>
2020-05-04 09:46:48 -0700

    changes to phones lambda

--- python_lambdas/price_scraper_lambda_phones.py
+++ python_lambdas/price_scraper_lambda_phones.py
@@ -12,9 +12,13 @@ def lambda_handler(event,context):
 
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                     'From': 'escalasa@oregonstate.edu'}
-        response = requests.get(reqs['url'], headers=headers)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
+        try:
+            response = requests.get(reqs['url'], headers=headers, verify=False, timeout=10)
+            soup = BeautifulSoup(response.text, 'html.parser')
+        except:
+            print("request timed out for " + reqs['url'])
+            
+       
         if reqs['vendor'] == "walmart":
             price = soup.find(class_=reqs['html_tag'])
             try:
@@ -23,24 +27,32 @@ def lambda_handler(event,context):
                 formatted_price = "unavailable"
         elif reqs['vendor'] == "newegg":
             data = soup.find_all(type=reqs['html_tag'])
-            json_data=json.loads(data[2].contents[0])
-            dictionary_data = json_data.get("offers")
-            #print(dictionary_data.get('price'))
-            price = dictionary_data.get('price')
-            formatted_price = price.get_text().strip()
+            try:
+                json_data=json.loads(data[2].contents[0])
+                dictionary_data = json_data.get("offers")
+                formatted_price = dictionary_data.get('price')
+            except:
+                formatted_price = "unavailable"
         elif reqs['vendor'] == "shopdevicesnow":
             price = soup.find(class_=reqs['html_tag'])
             formatted_price = price.get_text().strip()
         elif reqs['vendor'] == "ebay":
-            price = soup.find(id_=reqs['html_tag'])
-            formatted_price = price.get_text().strip()
+            price = soup.find(class_=reqs['html_tag'])
+            try:
+                formatted_price = price['content']
+            except:
+                formatted_price = "unavailable"
         elif reqs['vendor'] == "bhphotovideo":
             price = soup.find(class_=reqs['html_tag'])
             formatted_price = price.get_text().strip()
         elif reqs['vendor'] == "overseas":
-            price = soup.find(class_=reqs['html_tag'])
-            formatted_price = price['content']
-        elif reqs['vendor'] == "bestbuy": 
+            try:
+                price = soup.find(class_="PricesalesPrice")
+                formatted_price = price.span.get_text().strip()
+            except:
+                formatted_price = "unavailable"
+        elif reqs['vendor'] == "bestbuy":
+            formatted_price = "unavailable"
             '''
             #unsure how to incorporate this regex, blocker
             r = requests.get(url, headers = headers)
@@ -52,5 +64,5 @@ def lambda_handler(event,context):
  
     return {
         'statusCode': 200,
-        'body': str(results_obj)
+        'body': json.dumps(results_obj)
     }
