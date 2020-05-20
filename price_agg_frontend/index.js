346fa58d59f8ff9f9adf1cf1a0bed42fafdcdf74
moragaj <moragaj@oregonstate.edu>
2020-05-19 19:41:57 -0500

    refactoring changes

--- price_agg_frontend/index.js
+++ price_agg_frontend/index.js
@@ -9,19 +9,19 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    
+
     res.render('aggregator.handlebars')
-    
+
 });
 
-app.get('/monitor',function(req, res) {
-    
+function call_lambda(bucket, lambda_function, handlebars_page, req){
     const accesskey = process.env['AWSAccessKey']
     const secretkey = process.env['AWSSecretKey']
-    
+
     var AWS = require('aws-sdk');
     AWS.config.update({accessKeyId: accesskey
     , secretAccessKey: secretkey});
@@ -30,168 +30,48 @@ app.get('/monitor',function(req, res) {
     var s3 = new AWS.S3();
     var lambda = new AWS.Lambda();
     var options = {
-        Bucket : 'monitors-bb-361',
+    	Bucket : bucket,
         Key : req.query.parm,
         ResponseContentType : 'application/json'
     }
 
-     s3.getObject(options, function(err,data){
+    s3.getObject(options, function(err,data){
         var s3_response = data.Body.toString()
 
         var params = {
-            FunctionName: 'scrape_prices_monitors', /* required */
+            FunctionName: lambda_function, /* required */
             Payload: s3_response
-            };
-        
-            lambda.invoke(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                
-                var obj = JSON.parse(data.Payload)
-                var context = JSON.parse(obj.body)
+        };
 
-                brand_model = req.query.parm.replace(".json","")
-                context['product'] = brand_model.replace('/'," - ");
-        
-                res.render('monitor.handlebars',context)
-            });
-        
+        lambda.invoke(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+
+            var obj = JSON.parse(data.Payload)
+            var context = JSON.parse(obj.body)
+
+            brand_model = req.query.parm.replace(".json","")
+            context['product'] = brand_model.replace('/'," - ");
+            console.log(context);
+    		res.render(handlebars_page, context)
+        });
+
     });
+}
 
-
+app.get('/monitor',function(req, res) {
+    console.log(req.query.parm);
+    call_lambda('monitors-bb-361', 'scrape_prices_monitors', 'monitor.handlebars', req);
 })
 
 app.get('/cell_phone',function(req, res) {
-
     console.log(req.query.parm);
-    
-    const accesskey = process.env['AWSAccessKey']
-    const secretkey = process.env['AWSSecretKey']
-    
-    var AWS = require('aws-sdk');
-    AWS.config.update({accessKeyId: accesskey
-    , secretAccessKey: secretkey});
-    AWS.config.region = "us-east-1"
-
-    var s3 = new AWS.S3();
-    var lambda = new AWS.Lambda();
-    var options = {
-        Bucket : 'phones-bb-361',
-        Key : req.query.parm,
-        ResponseContentType : 'application/json'
-    }
-
-     s3.getObject(options, function(err,data){
-        var s3_response = data.Body.toString()
-
-        var params = {
-            FunctionName: 'scrape_prices_phones', /* required */
-            Payload: s3_response
-            };
-        
-            lambda.invoke(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-
-                var obj = JSON.parse(data.Payload)
-                var context = JSON.parse(obj.body)
-                brand_model = req.query.parm.replace(".json","")
-                context['product'] = brand_model.replace('/'," - ");
-        
-                res.render('phones.handlebars',context)
-            });
-        
-    });
-
-});
-
-app.get('/laptop',function(req, res) {
-
-    console.log(req.query.parm);
-
-    const accesskey = process.env['AWSAccessKey']
-    const secretkey = process.env['AWSSecretKey']
-
-    var AWS = require('aws-sdk');
-    AWS.config.update({accessKeyId: accesskey
-    , secretAccessKey: secretkey});
-    AWS.config.region = "us-east-1"
-
-    var s3 = new AWS.S3();
-    var lambda = new AWS.Lambda();
-    var options = {
-        Bucket : 'laptops-bb-361',
-        Key : req.query.parm,
-        ResponseContentType : 'application/json'
-    }
-
-     s3.getObject(options, function(err,data){
-        var s3_response = data.Body.toString()
-
-        var params = {
-            FunctionName: 'scrape_prices_laptops', /* required */
-            Payload: s3_response
-            };
-
-            lambda.invoke(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-
-                var obj = JSON.parse(data.Payload)
-                var context = JSON.parse(obj.body)
-
-                brand_model = req.query.parm.replace(".json","")
-                context['product'] = brand_model.replace('/'," - ");
-
-                res.render('laptop.handlebars',context)
-            });
-
-    });
-
-
-});
+    call_lambda('phones-bb-361', 'scrape_prices_phones', 'phones.handlebars', req);
+})
 
 app.get('/tv',function(req, res) {
-
     console.log(req.query.parm);
-
-    const accesskey = process.env['AWSAccessKey']
-    const secretkey = process.env['AWSSecretKey']
-
-    var AWS = require('aws-sdk');
-    AWS.config.update({accessKeyId: accesskey
-    , secretAccessKey: secretkey});
-    AWS.config.region = "us-east-1"
-
-    var s3 = new AWS.S3();
-    var lambda = new AWS.Lambda();
-    var options = {
-        Bucket : 'tvs-bb-361',
-        Key : req.query.parm,
-        ResponseContentType : 'application/json'
-    }
-
-     s3.getObject(options, function(err,data){
-        var s3_response = data.Body.toString()
-
-        var params = {
-            FunctionName: 'scrape_prices_tvs', /* required */
-            Payload: s3_response
-            };
-
-            lambda.invoke(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-
-                var obj = JSON.parse(data.Payload)
-                var context = JSON.parse(obj.body)
-
-                brand_model = req.query.parm.replace(".json","")
-                context['product'] = brand_model.replace('/'," - ");
-                res.render('tv.handlebars',context)
-
-            });
-
-    });
-
-
-});
+    call_lambda('tvs-bb-361', 'scrape_prices_tvs', 'tv.handlebars', req);
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
