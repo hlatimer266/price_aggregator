6b1ccc2583f64678e1a246f6d1a2a16c3486ec56
hlatimer266 <hlatimer266@gmail.com>
2020-05-24 19:29:35 -0700

    update package and docker run

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -6,4 +6,4 @@ cd price_agg_frontend/
 docker run -e AWSAccessKey=${AWSAccessKey} \
            -e AWSSecretKey=${AWSSecretKey} \
            -p 64333:3000 \
-           -d nodejs_cs361:latest
+           -d hlatimer266/nodejs_cs361:latest
--- bin/package_scraper.sh
+++ bin/package_scraper.sh
@@ -45,8 +45,8 @@ popd
 mv scrape_phones.zip scrape_monitors.zip scrape_tvs.zip scrape_laptops.zip python_lambdas
 
 pushd python_lambdas
-    zip -r scrape_monitors.zip price_scraper_lambda_mn.py
-    zip -r scrape_phones.zip price_scraper_lambda_phones.py
-    zip -r scrape_tvs.zip price_scraper_lambda_tvs.py
-    zip -r scrape_laptops.zip price_scraper_lambda_laptops.py
+    zip -r scrape_monitors.zip price_scraper_lambda_mn.py lowest_price.py
+    zip -r scrape_phones.zip price_scraper_lambda_phones.py lowest_price
+    zip -r scrape_tvs.zip price_scraper_lambda_tvs.py lowest_price
+    zip -r scrape_laptops.zip price_scraper_lambda_laptops.py lowest_price
 popd
